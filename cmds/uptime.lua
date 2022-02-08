8576d33e009a23a7edf5ee752ce801c5073c7fab
CoolingTool <CoolingTool@users.noreply.github.com>
2022-02-08 15:10:40 -0600

    fix last commit

--- cmds/uptime.lua
+++ cmds/uptime.lua
@@ -1,5 +1,5 @@
 local uv = require("uv")
 
 return function(message)
-    message:reply("started <t:".._G.starttime..":R> ||(<t:".._G.starttime..":>)||")
+    message:reply("started <t:".._G.starttime..":R> ||(or "..discordia.Time.fromSeconds(os.time() - _G.starttime):toString()..")||")
 end
